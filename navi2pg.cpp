8d99d06fb6833191468b5bce52349378de0a323e
Dmitry Baryshnikov <bishop.dev@gmail.com>
2014-03-15 04:22:23 +0400

    change Lso to Iso - isophase

--- navi2pg.cpp
+++ navi2pg.cpp
@@ -1056,7 +1056,7 @@ CPLString NAVI2PG::AddLightsSignatures::GetFirstPartOfSig(OGRFeature *srcFeature
         }
         case(7):
         {
-            litchrStr.Printf("lso%s", siggrpStr.c_str());
+            litchrStr.Printf("Iso%s", siggrpStr.c_str());
             break;
         }
         case(8):
